5164a45323d266fe3e9d053490e38d236ebd0a7c
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-11 12:44:49 +0800

    implements fetching of search results

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const Search = require('./model');
 
 const app = express();
@@ -9,7 +10,23 @@ const cxKey = process.env.CSE_CX;
 app.get('/api/imagesearch/:searchterm', (req, res) => {
   const offset = req.query.offset;
   const searchTerm = req.params.searchterm;
-  res.json({ searchTerm: searchTerm, offset });
+
+  const requestUrl = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cxKey}&q=${searchTerm}&searchType=image&start=${offset}`;
+  
+  axios.get(requestUrl).then((response) => {
+    const resultSet = response.data.items.map((item) => {
+      let newItem = {};
+      newItem.url = item.link;
+      newItem.snippet = item.snippet;
+      newItem.thumbnail = item.thumbnailLink;
+      newItem.context = item.image.contextLink;
+
+      return newItem;
+    });
+
+    res.json({ results: resultSet });
+  })
+  .catch((error) => console.log(error));
 });
 
 
